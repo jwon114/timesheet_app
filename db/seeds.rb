673d7604dfea9a7e64d65ccccc2222c69d552b30
jwon114 <jthgwong@gmail.com>
2019-07-05 01:01:11 +1000

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+timesheets = [
+  {
+    date: Date.today,
+    start_time: Time.parse("09:00").seconds_since_midnight,
+    finish_time: Time.parse("10:00").seconds_since_midnight
+  },
+  {
+    date: Date.yesterday,
+    start_time: Time.parse("12:00").seconds_since_midnight,
+    finish_time: Time.parse("17:00").seconds_since_midnight
+  },
+  {
+    date: Date.today - 1.week,
+    start_time: Time.parse("06:00").seconds_since_midnight,
+    finish_time: Time.parse("18:00").seconds_since_midnight
+  },
+  {
+    date: Date.today - 2.weeks,
+    start_time: Time.parse("11:00").seconds_since_midnight,
+    finish_time: Time.parse("23:00").seconds_since_midnight
+  }
+]
+
+timesheets.each { |ts| Timesheet.create(ts) }
